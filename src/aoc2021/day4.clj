969b0b6d8eef508ad270cef3df2a689cbfd5af79
peterhhchan <peterhhchan@gmail.com>
2021-12-04 05:19:40 -0500

    Day 4 - bugfix and code cleanup

--- src/aoc2021/day4.clj
+++ src/aoc2021/day4.clj
@@ -1,48 +1,42 @@
 (ns aoc2021.day4)
 
-(defn read-data []
-  (let [[n & lines] (->> (slurp "data/day4.txt")
-                          clojure.string/split-lines)]
-    {:numbers (->> (clojure.string/split n #",")
-                   (map #(Integer/parseInt %)))
-     :cards   (->> (partition 6 lines)
-                   (map rest)
-                   (map (fn [cards]
-                          (let [c (->> (mapv (fn [card]
-                                               (->> (clojure.string/split (clojure.string/trim card) #" +")
-                                                    (mapv #(Integer/parseInt %))))
-                                             cards))]
-                            (concat (map (partial into #{}) c)
-                                    (->> (apply map vector c)
-                                         (map (partial into #{}))))))))}))
+(defn str-to-ints [s]
+  (->> (re-seq #"\d+" s)
+       (map #(Integer/parseInt %))))
 
-(defn winner? [card]
-  (->> (map empty? card)
-       (some true?)))
+(defn read-data []
+  (let [[f & lines] (->> (slurp "data/day4.txt")
+                         clojure.string/split-lines)]
+    {:numbers (str-to-ints f)
+     :cards   (->> (map str-to-ints lines)
+                   flatten
+                   (partition 25)
+                   (map #(let [rows (partition 5 %)]
+                           (->> (apply map vector rows)
+                                (concat rows )
+                                (map (partial into #{}))))))}))
 
 (defn score [card number]
   (* number
-     (->> (flatten card)
-          (apply clojure.set/union)
+     (->> (apply clojure.set/union card)
           (reduce +))))
 
 (defn winners []
   (let [{:keys [numbers cards]} (read-data)]
-    (loop [cs cards
-           ns numbers
-           scores []]
-      (if (seq cs)
-        (let [updated (map (partial map #(disj % (first ns)) ) cs)
-              winner  (filter winner? updated)]
-          (if (seq winner)
-            (recur (remove winner? updated)
-                   (rest ns)
-                   (conj scores (score (first winner) (first ns))))
-            (recur updated (rest ns) scores)))
+    (loop [cs           cards
+           [num & nums] numbers
+           scores       (list)]
+      (if (and (seq cs) num)
+        (let [results (->> cs
+                           (map (partial map #(disj % num)))
+                           (group-by #(some true? (map empty? %))))]
+            (recur (results nil)
+                   nums
+                   (->> (results true)
+                        (map #(score % num) )
+                        (apply conj scores))))
         scores))))
 
-(defn part1 []
-  (first (winners)))
 
-(defn part2 []
-  (last (winners)))
+(def part1  (first (winners))) ;; 41503
+(def part2  (last  (winners))) ;; 3178
