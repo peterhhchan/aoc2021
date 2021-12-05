2e08529df661417225f3465932142754555f1df9
peterhhchan <peterhhchan@gmail.com>
2021-12-05 07:46:44 -0500

    Day 5 - another solution

--- src/aoc2021/day5.clj
+++ src/aoc2021/day5.clj
@@ -6,15 +6,16 @@
   (->> (re-seq #"\d+" s)
        (map parse-int)))
 
-(defn range-inclusive [s e]
-  (if (< s e)
-    (range s (inc e))
-    (range s (dec e) -1)))
-
 (defn parse-data []
   (->> (slurp "data/day5.txt")
        clojure.string/split-lines
        (map str-to-ints)))
+
+;; Solution 1 - handle diagonals differently, uses a map
+(defn range-inclusive [s e]
+  (if (< s e)
+    (range s (inc e))
+    (range s (dec e) -1)))
 
 (defn overlaps [count-diagonals?]
   (->> (parse-data)
@@ -33,5 +34,46 @@
        (apply merge-with + {})
        (filter (fn [[_ v]] (> v 1)))))
 
+
 (defn part1 [] (count (overlaps false)))
 (defn part2 [] (count (overlaps true)))
+
+;;
+;; Another solution - handle diagonals the same way, uses frequencies
+;;
+(defn step [a b]
+  (cond
+    (= a b) identity
+    (> a b) dec
+    (< a b) inc))
+
+(defn points-between [[ax ay bx by]]
+  (let [fx (step ax bx)
+        fy (step ay by)]
+    (->> [ax ay]
+         (iterate (fn [[x y]]
+                    [(fx x) (fy y)]))
+         (take-while (fn [[x y]]
+                       (not (and (= x (fx bx))
+                                 (= y (fy by)))))))))
+
+(defn diagonal? [[ax ay bx by]]
+  (not (or (= ax bx)
+           (= ay by))))
+
+(defn count-overlaps [pts]
+  (->> pts
+       (mapcat points-between)
+       (frequencies)
+       vals
+       (filter #(> % 1))
+       count))
+
+(defn part1-b []
+  (->> (parse-data)
+       (remove diagonal?)
+       (count-overlaps)))
+
+(defn part2-b []
+  (->> (parse-data)
+       (count-overlaps)))
