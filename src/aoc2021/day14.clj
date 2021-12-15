3da05eb1088b43e9d6ea3733716acc3ed8aa5fbb
peterhhchan <peterhhchan@gmail.com>
2021-12-15 01:54:41 -0500

    Day 15

--- src/aoc2021/day14.clj
+++ src/aoc2021/day14.clj
@@ -2,69 +2,72 @@
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-
-;; PHOSBSKBBBFSPPPCCCHN - 20
 (defn data [] (slurp "data/day14.txt"))
-(defn parse-long [s]  (Long/parseLong s))
-(defn str-to-ints [s]
-  (->> (re-seq #"\d" s)
-       (map parse-long)))
+(defn test-data []
+"NNCB
 
-(defn grow [s rules]
-  (let [combos   (->> (partition  2 1 s)
-                      (map (partial apply str))
-                      (map (fn [a]
-                             (str (first a) (rules a))))
-                      (apply str))]
-    (str combos (last s))))
-
-(defn part1 []
-  (let [d (->> (data))
-        [h r]  (str/split d #"\n\n")
-        rules     (->> (str/split-lines r)
-                       (map #(re-seq #"\w+" %))
-                       (map (fn [[a b]]
-                              {a b}))
-                       (into {}))
-        res (loop [n 9
-                   s h]
-              (if (pos? n)
-                (recur (dec n) (grow s rules))
-                (grow s rules)))]
-    (sort-by second (frequencies res))))
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C")
 
 
-(def freq-ss
-  (memoize
-   (fn [rules n ss]
-     (if (zero? n)
-       (frequencies ss)
-       (let [sss (rules ss)
-             s1  (subs sss 0 2)
-             s2  (subs sss 1)]
-         (->> [s1 s2]
-              (map (partial freq-ss rules (dec n)))
-              (apply merge-with + {(last s1) -1})))))))
-
-
-(defn freqs [ rules n ss]
-  (merge-with -
-              (->> (partition 2 1 ss)
-                   (map (comp  (partial freq-ss rules n)
-                               (partial str/join)))
-                   (reduce (partial merge-with +)))
-              (frequencies ss)
-              {(first ss) -1}
-              {(last ss) -1}))
-
-(defn part2 []
-  (let [d     (->> (data))
-        [h r] (str/split d #"\n\n")
-
+(defn parse-input [input]
+  (let [[h r] (str/split input #"\n\n")
         rules (->> (str/split-lines r)
                    (map #(re-seq #"\w+" %))
-                   (map (fn [[a b]]
-                          {a (str (first a) b (second a))}))
+                   (map (fn [[[a b] c]]
+                          {(str a b) [(str a c) (str c b)]}))
                    (into {}))]
-    (->> (freqs rules 40 h)
-         (sort-by second))))
+    {:template h
+     :rules    rules}))
+
+(defn step [rules polymer]
+  (reduce-kv (fn [p k v]
+               (let [[a b] (rules k)]
+                 (-> p
+                     (update k (fnil - 0) v )
+                     (update a (fnil + 0) v)
+                     (update b (fnil + 0) v))))
+             polymer
+             polymer))
+
+(defn count-freqs [template freqs]
+  (->> freqs
+       (map (fn [[[a b] v]]
+              {b v}))
+       (apply merge-with + {(first template) 1})))
+
+(defn solve [input steps]
+  (let [{:keys [template rules]} (parse-input input)]
+    (->> (partition 2 1 template)
+         (map str/join)
+         (frequencies)
+         (iterate (partial step rules))
+         (drop steps)
+         first
+         (count-freqs template)
+         (map second)
+         sort
+         ((juxt last first))
+         (reduce -))))
+
+;; 2874
+(defn part1 [input]
+  (solve input 10))
+
+;; 5208377027195
+(defn part2 [input]
+  (solve input 40))
---
+++ src/aoc2021/day15.clj
@@ -0,0 +1,91 @@
+(ns aoc2021.day15
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse-long [s]  (Long/parseLong s))
+(defn data [] (slurp "data/day15.txt"))
+
+(defn parse-input []
+  (->> (data)
+       ;; test-data
+       (str/split-lines)
+       (map #(re-seq #"\d" %))
+       (mapv #(mapv parse-long %))))
+
+(defn neighbors [n [x y]]
+  (->>
+   [[(inc x) y]
+    [x (inc y)]
+    [x (dec y)]
+    [(dec x) y]]
+   (filter (fn [[x y]]
+             (and (<= 0 y (dec n))
+                  (<= 0 x (dec n)))))))
+
+(defn my-inc [d]
+  (if (> (inc d) 9)
+    1
+    (inc d)))
+
+(defn expand [n input]
+  (->> input
+       (map (fn [row]
+              (->> (iterate #(map my-inc %) row)
+                   (take n)
+                   (apply concat))))
+       (iterate (fn [rows]
+                  (map #(map my-inc %) rows)))
+       (take n)
+       (apply concat)
+       (mapv vec)))
+
+(defn candidates [board size costs cur]
+  (->> (neighbors size cur)
+       (filter (fn [next]
+                 (or (nil? (costs next))
+                     (> (costs next) (+ (board next) (costs cur))))))))
+
+(defn min-cost [coords]
+  (let [size  (count coords)
+        board (reduce (fn [m pos]
+                        (assoc m pos (get-in coords pos)))
+                      {}
+                      (for [x (range size) y (range size)] [x y]))]
+    (loop [costs    {[0 0] 0}
+           to-check (neighbors size [0 0])]
+      (if (seq to-check)
+        (let [updated-costs (reduce (fn [m pos]
+                                  (->> (neighbors size pos)
+                                       (keep m)
+                                       (apply min)
+                                       (+ (board pos))
+                                       (assoc m pos)))
+                                costs
+                                to-check)
+
+              to-check-next (->> to-check
+                                 (mapcat #(candidates board size updated-costs %) )
+                                 set)]
+          (recur updated-costs to-check-next))
+        (get costs [(dec size) (dec size)])))))
+
+;;741
+(defn part1 []
+  (min-cost (parse-input)))
+
+;; 2976
+(defn part2 []
+  (min-cost (expand 5 (parse-input))))
+
+
+(def test-data
+"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581")
