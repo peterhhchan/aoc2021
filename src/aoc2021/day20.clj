e5beae39e78c35aabb17bfb38201641dfa00b5a1
peterhhchan <peterhhchan@gmail.com>
2022-01-02 07:39:22 -0500

    Day 20 code cleanup

--- src/aoc2021/day20.clj
+++ src/aoc2021/day20.clj
@@ -2,21 +2,9 @@
   (:require [clojure.string :as str]
             [clojure.zip :as zip]))
 
-(defn data [] (slurp "data/day20.txt"))
-(defn test-data []
-"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##
-#..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###
-.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#.
-.#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#.....
-.#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#..
-...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....
-..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+;; --- Day 20: Trench Map ---
 
-#..#.
-#....
-##..#
-..#..
-..###")
+(defn data [] (slurp "data/day20.txt"))
 
 (defn convert [s]
   (-> s
@@ -24,54 +12,53 @@
       (str/replace  "#" "1")
       (str/join)))
 
+(def char->int {\1 1 \0 0})
+
 (defn parse-input []
-  (let [[a b]  (str/split (data) #"\n\n")]
-    [(vec  (convert (str/join (str/split-lines a))))
-     (str/split-lines (convert b))]))
+  (let [[alg image]  (str/split (data) #"\n\n")]
+    [(->> (str/split-lines alg)
+          (str/join)
+          (convert)
+          (map char->int)
+          vec)
+     (str/split-lines (convert image))]))
 
-
-(defn neighbors [[x y]]
+(defn one-away [[x y]]
   (for [a [-1 0 1]
         b [-1 0 1]]
     [(+ a  x) (+ b y)]))
 
-;; Can also do neighbors of neighbors
-(defn expand [[x y]]
-  (for [a (range -2 3)
-        b (range -2 3)]
+(defn two-away [[x y]]
+  (for [a [-2 -1 0 1 2]
+        b [-2 -1 0 1 2]]
     [(+ a x) (+ b y)]))
 
 (defn dark-points [pts]
-  (->>  (filter #(= 1 (second %)) pts)
-        (map first)
-        set))
+  (filter #(= 1 (second %)) pts))
 
-(defn flash [n]
-  (let [[decoder b] (parse-input)
-        light-value {\1 1 \0 0}
-        grid        (mapv vec b)]
-    (loop [pts (->> (for [x (range (count grid))
-                          y (range (count grid))]
-                      {[x y] (-> (get-in grid [x y])
-                                 (light-value ))})
-                    (into {}))
-           n   n]
+(defn flash [steps]
+  (let [[decoder image] (parse-input)
+        grid            (mapv vec image)]
+    ;; Maintain a set of points we have explored, points not within
+    ;; this set alternate between 0 and 1.
+    (loop [explored (->> (for [x (range (count grid))
+                               y (range (count grid))]
+                           {[x y] (-> (get-in grid [x y])
+                                      (char->int))})
+                         (into {}))           n steps]
       (if (zero? n)
-        (count (dark-points pts))
-        (recur
-         (->> pts
-              (filter #(= 1 (second %)))
-              (map (comp set expand first))
-              (apply clojure.set/union)
-              (pmap (fn [p]
-                      {p (->> (neighbors p)
-                              (map #(get pts % (if (even? n) \0 \1)) )
-                              (apply str "2r")
-                              (read-string)
-                              (decoder)
-                              light-value)}))
-              (into {}))
-         (dec n))))))
+        (count (dark-points explored))
+        (let [light-or-dark (if (even? n) \0 \1)]
+          (recur (->> (dark-points explored)
+                      (map (comp set two-away first))
+                      (apply clojure.set/union)
+                      (into {} (map (fn [p]
+                                      {p (->> (one-away p)
+                                              (map #(get explored % light-or-dark))
+                                              (apply str "2r")
+                                              (read-string)
+                                              (decoder))}))))
+                 (dec n)))))))
 
 ;; 4928
 (defn part1 [] (flash 2))
